f95d0681316853ee763d77d3e2ca8137826eb9a4
leahcim132 <michael_lum2006@outlook.com>
2015-10-13 14:03:26 -0700

    Create obj-exercise.js
    Print out the numbers from 1 - 20.
    ----------
    The rules:
    1.For numbers divisible by 3, print out "Fizz".
    2. For numbers divisible by 5, print out "Buzz".
    3. For numbers divisible by both 3 and 5, print out "FizzBuzz" in the console.
    4. Otherwise, just print out the number.

---
+++ JavaScript/obj-exercise.js
@@ -0,0 +1,28 @@
+for(var i = 1 ; i <= 20 ; i++){
+    if( i%3 == 0)
+    {
+        if((i%3 == 0) && (i%5 == 0)){
+            console.log("FizzBuzz");
+        }
+        else{
+            console.log("Fizz");
+        }
+    
+    }
+    
+    else if(i%5 == 0)
+    {
+        if((i%3 == 0) && (i%5 == 0)){
+            console.log("FizzBuzz");
+        }
+        else{
+            console.log("Buzz");
+        }
+    }
+      
+    else{
+        console.log(i);
+    }
+
+
+}
